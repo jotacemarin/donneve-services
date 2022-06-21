03dce3f0f4c2e2a9cace199f101fa42f67d5efb6
jotacemarin <jotacemarin@gmail.com>
2022-06-20 19:46:30 -0500

    added mixpanel to daily participation

--- src/daily_participation/index.js
+++ src/daily_participation/index.js
@@ -2,26 +2,62 @@
 
 const { connect, userModel } = require("../persistence");
 const { publicWebhook } = require("../utils/botnorrea");
+const { getInsights } = require("../utils/mixpanel");
 const { createResponse, createErrorResponse } = require("../utils/parser");
 
 const DISABLED = true;
 
+const sanitizeUser = (element) => {
+  const { username, id } = element;
+  return { username, id };
+};
+
+const getUsers = (series) => {
+  const [key] = Object.keys(series);
+  const users = Object.keys(series[key]);
+  return users.map((user) => ({ id: `${user}` }));
+};
+
+const populateUser = (rawReport, users) => (key) => {
+  const user = users.find((user) => user.id === key);
+  const { value } = rawReport[key];
+  return { ...user, value };
+};
+
+const populateUsers = (rawReport, users = []) => {
+  return Object.keys(rawReport).map(populateUser(rawReport, users));
+};
+
 const calculateStars = (total, score) => {
-  const percent = (score * 100) / total + 5;
-  const frogs = Math.floor(percent / 10);
+  const rawPercent = (score * 100) / total + 5;
+  const rawFrogs = Math.floor(rawPercent / 10);
+  const percent = Number(rawPercent).toFixed(2);
+  const frogs = rawFrogs ? rawFrogs : 1;
   return { percent, frogs };
 };
 
-const sanitizeObject = (element) => {
-  const { username, score } = element;
-  return { username, score };
+const calculatePercent = (total) => (element) => {
+  const { value } = element;
+  const { frogs, percent } = calculateStars(total, value);
+  return { ...element, frogs, percent };
 };
 
-const calculatePercent = (total, totalTop) => (element) => {
-  const { score } = element;
-  const { percent } = calculateStars(total, score);
-  const { frogs } = calculateStars(totalTop, score);
-  return { ...element, percent, frogs };
+const getSumReports = (series, users) => {
+  const [messagesKeys, wordsKeys] = Object.keys(series);
+
+  const reportMessages = series[messagesKeys];
+  const reportWords = series[wordsKeys];
+
+  const messages = populateUsers(reportMessages, users);
+  const words = populateUsers(reportWords, users);
+
+  const [{ value: overallMessages }] = messages.splice(messages.length - 1, 1);
+  const [{ value: overallWords }] = words.splice(words.length - 1, 1);
+
+  const messagesPercent = messages.map(calculatePercent(overallMessages));
+  const wordsPercent = words.map(calculatePercent(overallWords));
+
+  return { messages: messagesPercent, words: wordsPercent };
 };
 
 const stringBuilder = (element) => {
@@ -37,38 +73,37 @@ const dailyParticipation = async (_event, context, callback) => {
   try {
     await connect();
 
-    const $match = {};
-    const [{ total: participation }] = await userModel
-      .aggregate([
-        { $match },
-        {
-          $group: {
-            _id: "participation",
-            total: {
-              $sum: "$score",
-            },
-          },
-        },
-      ])
-      .exec();
-    const topFiveMetric = await userModel
-      .find($match, { _id: 0, username: 1, score: 1 })
-      .sort({ score: -1 })
-      .limit(5)
-      .exec();
-    const topFiveSanitized = topFiveMetric.map(sanitizeObject);
-    const onlyTopFive = topFiveSanitized
-      .map(({ score }) => score)
-      .reduce((previous, current) => previous + current, 0);
-    const topFive = topFiveSanitized
-      .map(calculatePercent(participation, onlyTopFive))
-      .map(stringBuilder);
+    const [{ series }] = await getInsights();
+    const users = getUsers(series);
 
-    const message = `Botnorrea top 5 sapometro:\n\n${topFive.join("\n")}`;
-    const { status } = DISABLED
-      ? { status: false }
-      : await publicWebhook({ message });
-    return callback(null, createResponse({ message, botnorrea: { status } }));
+    const $match = { $or: users };
+    const usernames = await userModel
+      .find($match, { _id: 0, username: 1, id: 1 })
+      .exec()
+      .then((response) => response.map(sanitizeUser));
+
+    const { messages, words } = getSumReports(series, usernames);
+    const stringMessages = messages.map(stringBuilder).join("\n");
+    const stringWords = words.map(stringBuilder).join("\n");
+
+    const toMessages = `Botnorrea sapometro by messages:\n\n${stringMessages}`;
+    const toWords = `Botnorrea sapometro by words:\n\n${stringWords}`;
+
+    const responses = await Promise.all([
+      publicWebhook({ message: toMessages }),
+      publicWebhook({ message: toWords }),
+    ]);
+    const [{ status: statusMessages }, { status: statusWords }] = responses;
+
+    const completeMessage = `${toMessages}\n\n${toWords}`;
+
+    return callback(
+      null,
+      createResponse({
+        message: completeMessage,
+        botnorrea: { statusMessages, statusWords },
+      })
+    );
   } catch (error) {
     const { message } = error;
     console.error("dailyImage: ", message);
---
+++ src/utils/mixpanel.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const {
+  MIX_PANEL_API,
+  MIX_PANEL_USERNAME,
+  MIX_PANEL_USERNAME_SECRET,
+  MIX_PANEL_PROJECT_ID,
+  MIX_PANEL_REPORTS,
+} = process.env;
+
+const axios = require("axios").default;
+
+const reports = String(MIX_PANEL_REPORTS).split(",");
+
+const botnorrea = axios.create({
+  baseURL: MIX_PANEL_API,
+  timeout: 5000,
+});
+
+const getInsight = async (report) => {
+  const auth = {
+    username: MIX_PANEL_USERNAME,
+    password: MIX_PANEL_USERNAME_SECRET,
+  };
+  const params = { project_id: MIX_PANEL_PROJECT_ID, bookmark_id: report };
+
+  const { data } = await botnorrea.get("/insights", { auth, params });
+  const { date_range, series } = data;
+  return { dateRange: date_range, series };
+};
+
+const getInsights = async () => {
+  const allResponse = await Promise.all(reports.map(getInsight));
+  return allResponse;
+};
+
+module.exports = {
+  getInsights,
+};
